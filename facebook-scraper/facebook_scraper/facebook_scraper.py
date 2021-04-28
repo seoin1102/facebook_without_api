21a4564d97b7e75b56f0fbd3c7a534dd03fdc52c
seoin1102 <69146576+seoin1102@users.noreply.github.com>
2021-04-28 17:41:42 +0900

    Bug fix
    Duplicate influencers can be distinguished and stored.

--- facebook-scraper/facebook_scraper/facebook_scraper.py
+++ facebook-scraper/facebook_scraper/facebook_scraper.py
@@ -1,199 +1,216 @@
-import itertools
-import logging
-import warnings
-from functools import partial
-from typing import Iterator, Union
+import json
+import base64
+import requests
+from datetime import datetime
+from time import sleep
+from facebook_scraper import get_posts
+from IPython.core.events import post_run_cell
 
-from requests import RequestException
-from requests_html import HTMLSession
-
-from . import utils
-from .constants import DEFAULT_PAGE_LIMIT, FB_BASE_URL, FB_MOBILE_BASE_URL
-from .extractors import extract_group_post, extract_post
-from .fb_types import Post, Profile
-from .page_iterators import iter_group_pages, iter_pages
-
-
-logger = logging.getLogger(__name__)
-
-
-class FacebookScraper:
-    """Class for creating FacebookScraper Iterators"""
-
-    base_url = FB_MOBILE_BASE_URL
-    user_agent = (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/76.0.3809.87 Safari/537.36"
-    )
-    default_headers = {
-        'User-Agent': user_agent,
-        'Accept-Language': 'en-US,en;q=0.5',
+token = '34ea8abce853ae9a90d4a826c52766aadfe591d9'
+headers= {'Authorization': 'token ' + token,
+    'Accept': 'application/json',
+    'Content-Type': 'application/json;charset=UTF-8'
     }
 
-    def __init__(self, session=None, requests_kwargs=None):
-        if session is None:
-            session = HTMLSession()
-            session.headers.update(self.default_headers)
+def influencer_check():
+    url ='http://test.dabi-api.com/api/influencer/'
+    i=0
+    facebook_id_list = []
+    facebook_pk_list = {}
+    t = True
+    while t:
+        sleep(10)
+        print('influencer_check')
+        response = requests.get(url+'?offset='+str(i), headers=headers)
+        print(response)
+        print(i)
 
-        if requests_kwargs is None:
-            requests_kwargs = {}
+        if response:
+            try:
+                data = response.json()
+            except Exception as e:
+                print('Invalid response data in influencer_pk')
+                continue
+            if data:
+                # We should use .get when we have a dictionary for safety reason
+                result = data.get('results', None)
+                count = data.get('count')
+                # Check result is in correct form
+                if result:        
+                    for i2 in range(len(result)):
+                        
+                        if result[i2]['pk'] != None:
+                            facebook_pk_list[result[i2]['insta_pk']] = result[i2]['pk']
+                            facebook_id_list.append(result[i2]['insta_pk'])
+                        if data['next']==None and count == len(facebook_pk_list):
+                            
+                            if result[i2]['pk'] != None:
+                                facebook_pk_list[result[i2]['insta_pk']] = result[i2]['pk']
+                                facebook_id_list.append(result[i2]['insta_pk'])
+                            with open('dosiciety.json','rt', encoding='UTF8') as json_file:
+                                json_data = json.load(json_file)
+                                id_li = []
+                                for i in range(len(json_data)):
+                                    influencer_id = json_data[i]['user_id']
+                                    id_li.append(influencer_id)
 
-        self.session = session
-        self.requests_kwargs = requests_kwargs
+                                for i in range(len(json_data)):
+                                    influencer = json_data[i]['username']
+                                    influencer_id = json_data[i]['user_id']
+                                    
+                                    profile_pic  = 'https://scontent-gmp1-1.xx.fbcdn.net/v/t1.18169-1/c0.16.148.116a/p148x148/11817213_10155879004550352_2443853221089824039_n.png?_nc_cat=1&ccb=1-3&_nc_sid=0c64ff&_nc_ohc=NAwfBdBbEykAX-_6niJ&_nc_ht=scontent-gmp1-1.xx&tp=30&oh=981c25a67f68d2fbfefd1cf69bcf06d3&oe=609A61A3'
+                                    base64_bytes = base64.b64encode(requests.get(profile_pic).content)
+                                    image_url = base64_bytes.decode('utf-8')
 
-    def get_posts(self, account: str, **kwargs) -> Iterator[Post]:
-        iter_pages_fn = partial(iter_pages, account=account, request_fn=self.get)
-        return self._generic_get_posts(extract_post, iter_pages_fn, **kwargs)
+                                    if influencer_id not in facebook_id_list:
+                                        print(influencer)
+                                        k = len(facebook_pk_list)-1
+                                        print('New influencer')
+                                        print(requests.post('http://test.dabi-api.com/api/influencer/', json={"insta_id": influencer, "insta_pk":influencer_id , "profile_image": image_url}, headers=headers))
+                                        sleep(13)
+                                        response = requests.get(url+'?offset='+str(k), headers=headers)
+                                        data = response.json()
+                                        result = data.get('results', None)
+                                        facebook_pk_list[result[-1]['insta_pk']] = result[-1]['pk']
+                                        facebook_id_list.append(result[-1]['insta_pk'])
 
-    def get_posts_by_url(self, post_urls, options={}, remove_source=True) -> Iterator[Post]:
-        for post_url in post_urls:
-            if post_url.startswith(FB_BASE_URL):
-                post_url = post_url.replace(FB_BASE_URL, FB_MOBILE_BASE_URL)
-            if not post_url.startswith(FB_MOBILE_BASE_URL):
-                post_url = utils.urljoin(FB_MOBILE_BASE_URL, post_url)
-            logger.debug(f"Requesting page from: {post_url}")
-            response = self.get(post_url)
-            elem = response.html.find('article[data-ft],div.async_like[data-ft]', first=True)
-            if not elem:
-                logger.warning("No raw posts (<article> elements) were found in this page.")
-            post = extract_post(elem, request_fn=self.get, options=options)
-            if remove_source:
-                post.pop('source', None)
-            yield post
+                                    A = set(facebook_id_list) & set(id_li)
+                                    B = set(id_li)
+                                    
+                                    if A == B:
+                                        return facebook_pk_list    
+                            # t = False
+                    i+=20
+                             
+    
 
-    def get_profile(self, account, **kwargs) -> Profile:
-        about_url = utils.urljoin(FB_MOBILE_BASE_URL, f'/{account}/about/')
-        logger.debug(f"Requesting page from: {about_url}")
-        response = self.get(about_url)
-        result = {}
-        # Profile name is in the title
-        title = response.html.find("title", first=True).text
-        if " | " in title:
-            title = title.split(" | ")[0]
-        result["Name"] = title
+facebook_pk_list = influencer_check()
+def check_post(influencer_id):
+    print('check post')
+    post_list=[]
+    pk = facebook_pk_list[str(influencer_id)]
+    sleep(10)
+    response = requests.get('http://test.dabi-api.com/api/influencer/'+str(pk)+'/feedback/?is-active=all', headers=headers)
 
-        about = response.html.find("div#main_column,div.aboutme", first=True)
-        if not about:
-            logger.warning("No about section found")
-            return result
-        for card in about.find("div[data-sigil='profile-card']"):
-            header = card.find("header", first=True).text
-            if header.startswith("About"):
-                header = "About" # Truncate strings like "About Mark"
-            if header in ["Work, Education"]:
-                experience = []
-                for elem in card.find("div.experience"):
-                    xp = {}
-                    try:
-                        xp["link"] = elem.find("a", first=True).attrs["href"]
-                    except:
-                        pass
-                    bits = elem.text.split("\n")
-                    if len(bits) == 2:
-                        xp["text"], xp["type"] = bits
-                    elif len(bits) == 3:
-                        xp["text"], xp["type"], xp["year"] = bits
-                    else:
-                        xp["text"] = elem.text
-                    experience.append(xp)
-                result[header] = experience
-            elif header == "Places lived":
-                places = []
-                for elem in card.find("div.touchable"):
-                    place = {}
-                    try:
-                        place["link"] = elem.find("a", first=True).attrs["href"]
-                    except:
-                        pass
-                    if "\n" in elem.text:
-                        place["text"], place["type"] = elem.text.split("\n")
-                    else:
-                        place["text"] = elem.text
-                    places.append(place)
-                result[header] = places
-            else:
-                bits = card.text.split("\n")[1:] # Remove header
-                if len(bits) >= 3 and header == "Relationship":
-                    result[header] = {
-                        "to": bits[0],
-                        "type": bits[1],
-                        "since": bits[2]
-                    }
-                elif len(bits) == 1:
-                    result[header] = bits[0]
-                elif header in ["Contact Info", "Basic info", "Other names"] and len(bits) % 2 == 0: # Divisible by two, assume pairs
-                    pairs = {}
-                    for i in range(0, len(bits), 2):
-                        pairs[bits[i + 1]] = bits[i]
-                    result[header] = pairs
-                else:
-                    result[header] = "\n".join(bits)
-        return result
-
-    def get_group_posts(self, group: Union[str, int], **kwargs) -> Iterator[Post]:
-        iter_pages_fn = partial(iter_group_pages, group=group, request_fn=self.get)
-        return self._generic_get_posts(extract_group_post, iter_pages_fn, **kwargs)
-
-    def get(self, url, **kwargs):
+    if response:
         try:
-            response = self.session.get(url=url, **self.requests_kwargs, **kwargs)
-            response.html.html = response.html.html.replace('<!--', '').replace('-->', '')
-            response.raise_for_status()
-            return response
-        except RequestException as ex:
-            logger.exception("Exception while requesting URL: %s\nException: %r", url, ex)
-            raise
+            data = response.json()
+        except Exception as e:
+            print('Invalid response data in check_post')
+        if data:
+            result = data.get('results')
+            if result:
+                for i in range(len(result)):
+                    post_list.append(result[i]['post_id'])
+                return post_list
 
-    def login(self, email: str, password: str):
-        login_page = self.get(self.base_url)
-        login_action = login_page.html.find('#login_form', first=True).attrs.get('action')
+def post_pk(pk1, post_id):
+    print('find pk')
+    url =f'http://test.dabi-api.com/api/influencer/{pk1}/feedback/?is-active=all'
+    i=0
+    while 1:
+        sleep(10)
+        response = requests.get(url+'&offset='+str(i), headers=headers)
+        if response:
+            try:
+                data = response.json()
+            except Exception as e:
+                print('Invalid response data in post_pk')
+                continue
+            if data:
+                result = data.get('results', None)
+                sleep(1)
+                if result:
+                    for i2 in range(len(result)):
+                        if post_id == result[i2]['post_id']:
+                            pk = result[i2]['pk']
+                            return pk
+                        else:
+                            print('No post')
+                            i+=20
 
-        response = self.session.post(
-            utils.urljoin(self.base_url, login_action), data={'email': email, 'pass': password}
-        )
-        response_text = response.html.find('#viewport', first=True).text
+with open('dosiciety.json','rt', encoding='UTF8') as json_file:
 
-        logger.debug("Login response text: %s", response_text)
+    json_data_1 = json.load(json_file)
+    id_list_1 = []     
+    id_list = [] 
+    json_data = []
 
-        login_error = response.html.find('#login_error', first=True)
-        if login_error:
-            logger.error("Login error: %s", login_error.text)
+    for i in range(len(json_data_1)):
+        if json_data_1[i]['image'] != None:
+            if "https://m.facebook.com/photo/" in json_data_1[i]['image']:
+                continue
+        if json_data_1[i]['image']== None and json_data_1[i]['video_thumbnail']==None:
+            continue  
+        if json_data_1[i]['post_id'] == None:
+            continue 
+        else: 
+            json_data.append(json_data_1[i])
 
-        if 'c_user' not in self.session.cookies:
-            warnings.warn('login unsuccessful')
+    for i in range(len(json_data)):
 
-    def is_logged_in(self) -> bool:
-        response = self.get('https://m.facebook.com/settings')
-        return not response.url.startswith('https://m.facebook.com/login.php')
+        influencer_id = json_data[i]['user_id']
+        pk1 = facebook_pk_list[str(influencer_id)]
+        url = json_data[i]['post_url']
+        post_id = json_data[i]['post_id']
+        id_list = check_post(influencer_id)
+        if id_list == None:
+            id_list = []
 
-    def _generic_get_posts(
-        self,
-        extract_post_fn,
-        iter_pages_fn,
-        page_limit=DEFAULT_PAGE_LIMIT,
-        options=None,
-        remove_source=True,
-    ):
-        counter = itertools.count(0) if page_limit is None else range(page_limit)
+        if post_id in id_list:
+            i+=1
+            print('Already on the list')
 
-        if options is None:
-            options = {}
-        elif isinstance(options, set):
-            warnings.warn("The options argument should be a dictionary.", stacklevel=3)
-            options = {k: True for k in options}
+        elif post_id not in id_list:
+            id_list_1.append(post_id)
+            print(id_list_1)
+            if json_data[i]['image'] != None:
+                image_url = json_data[i]['image']
+            elif json_data[i]['image'] == None:
+                image_url = json_data[i]['video_thumbnail']
+                
+            base64_bytes = base64.b64encode(requests.get(image_url).content)
+            image_url = base64_bytes.decode('utf-8')
 
-        if page_limit and page_limit <= 2:
-            warnings.warn(
-                "A low page limit (<=2) might return no results, try increasing the limit",
-                stacklevel=3,
-            )
+            date_posted_human = json_data[i]['time']
+            date_posted_human = datetime.fromtimestamp(date_posted_human).strftime("%Y-%m-%d")
+            captions = json_data[i]['text']                   
+            sleep(10)              
+            print('New post')
+            print(requests.post(f'http://test.dabi-api.com/api/influencer/{pk1}/feedback/', json={"post_url": url, "post_id": post_id, "post_thumb_image":image_url ,"post_taken_at_timestamp": date_posted_human,
+                                    "post_description": captions}, headers=headers))  
+            
 
-        logger.debug("Starting to iterate pages")
-        for i, page in zip(counter, iter_pages_fn()):
-            logger.debug("Extracting posts from page %s", i)
-            for post_element in page:
-                post = extract_post_fn(post_element, options=options, request_fn=self.get)
-                if remove_source:
-                    post.pop('source', None)
-                yield post
+            if json_data[i]['images'] != None:
+                
+                for k in json_data[i]['images']:
+                    
+                    k.replace(r'\u0026', "&")
+                    base64_bytes = base64.b64encode(requests.get(k).content)
+                    display_url = base64_bytes.decode('utf-8')
+                    sleep(1)
+                    pk2 = post_pk(pk1, post_id)
+                    sleep(10)
+    
+                    print(requests.post(f'http://test.dabi-api.com/api/influencer/{pk1}/feedback/{pk2}/image', json={"source": display_url}, headers=headers)) 
+        
+            if url != None:
+                li = [0]
+                li[0] = url
+                print(li)
+                
+                posts = list(get_posts(post_urls = li, options={'comments': True}, cookies="cookies.txt", timeout=15))
+                sleep(10)
+
+                if posts[0]['comments_full'] == None:
+                    i+=1
+
+                if posts[0]['comments_full'] != None:
+                    for post in posts[0]['comments_full']:
+                        comments = post['comment_text']  
+                        sleep(10)
+                        print(requests.post(f'http://test.dabi-api.com/api/influencer/posts/{pk2}/comments/', json={"text": comments}, headers=headers))   
+                        print("text", comments)   
+                
+                    
+
